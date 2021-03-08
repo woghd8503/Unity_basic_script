08e9d677e7e07da4117217fe17a02623c9c33138
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-03-09 01:53:26 +0900

    Unity_basic_script

---
+++ Unity_basic_script/Assets/Script/Unity_basic1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity_basic1 : MonoBehaviour
+{
+    public int myNumber = 19;
+    public int number1 = 2;
+    public int number2 = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(2 + 9);
+
+        Debug.Log(11 + myNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return)) AddTwoNumbers();
+    }
+
+    private void AddTwoNumbers()
+    {
+        Debug.Log(number1 + number2);
+    }
+}
---
+++ Unity_basic_script/Assets/Script/Unity_basic2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity_basic2 : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("Awake Function is called");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start function is called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update function is called");
+    }
+}
