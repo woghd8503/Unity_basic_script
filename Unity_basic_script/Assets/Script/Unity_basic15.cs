bf50947b6935d6b48f6f2e14522915fd6d0d1140
PJH <woghd8503@naver.com>
2021-03-15 00:31:21 +0900

    while

---
+++ Unity_basic_script/Assets/Script/Unity_basic15.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity_basic15 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+      while(i < 10)
+        {
+            Debug.Log(i);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
